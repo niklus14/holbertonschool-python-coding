64d95ce3b68eaa7073dacd7fc2c19409248ec1f9
niklus14 <ssenkrad14@gmail.com>
2025-11-23 19:48:25 +0000

    classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
+"""
+this module defines a class Square with input validation and area calculation.
+it demonstrates the use of public methods to expose private data behavior.
+"""
+
+
 class Square:
+    """
+    it represents a square with strict size validation.
+
+    attributes:
+        __size (int): the size of the square (private).
+    """
     def __init__(self, size=0):
+        """
+        initialise the square with type and value checks.
+
+        args:
+            size (int): the size of the square.defaults to 0.
+
+        raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -8,4 +30,10 @@ class Square:
         self.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        """
+        calculates and returns the area of the square.
+
+        returns:
+            int: the area of the square (size + size).
+        """
+        return self.__size ** 2
